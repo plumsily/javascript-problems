6681e3c6dedd16341ba970b058c8d5d806c4e2aa
Justin Lee <38745924+plumsily@users.noreply.github.com>
2022-11-24 15:42:23 -0800

    daily leetcode

---
+++ LeetCode/383-ransom-note.js
@@ -0,0 +1,33 @@
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+// Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+// Constraints:
+
+// 1 <= ransomNote.length, magazine.length <= 105
+// ransomNote and magazine consist of lowercase English letters.
+
+// /**
+//  * @param {string} ransomNote
+//  * @param {string} magazine
+//  * @return {boolean}
+//  */
+
+//p: magazine is a string that should contain all the letters needed to make ransomNote.
+//r: we must return a boolean true or false based on if all the letters to make ransomNote can be taken from magazine, but only once per letter.
+//e: in the above examples, we see that all letters in ransomNote is unique and must be mapped to a letter in magazine. thus, there can be no repeated letters in ransomNote from one letter in magazine. letters can be unused in magazine as long as all the letters for ransomNote is found.
+//p:
+var canConstruct = function (ransomNote, magazine) {};
