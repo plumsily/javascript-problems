9ab49e9925d8396407bebcb27ebcc69bfebc1ab5
Justin Lee <38745924+plumsily@users.noreply.github.com>
2022-11-28 17:53:00 -0800

    daily leetcode

--- LeetCode/383-ransom-note.js
+++ LeetCode/383-ransom-note.js
@@ -30,23 +30,74 @@
 //r: we must return a boolean true or false based on if all the letters to make ransomNote can be taken from magazine, but only once per letter.
 //e: in the above examples, we see that all letters in ransomNote is unique and must be mapped to a letter in magazine. thus, there can be no repeated letters in ransomNote from one letter in magazine. letters can be unused in magazine as long as all the letters for ransomNote is found.
 //p: we construct a hash table, which contains key value pairs for the letter and index for magazine based on the letters in ransomNote. we reject adding values into the hashtable if the index is repeated, and pass false. if the hashtable is the same length as ransomNote, then we know all letters can be traced to magazine uniquely and pass true.
+//alternatively, we can create a hashmap out of magazine and subtract character counts from it as we compare it to ransomNote.
 var canConstruct = function (ransomNote, magazine) {
-  let hashTable = {};
-  for (let i = 0; i < magazine.length; i++) {
-    if (ransomNote.includes(magazine[i])) {
-      if (!Object.hasOwn(hashTable, i)) {
-        hashTable[i] = ransomNote[i];
-      } else {
-        return false;
-      }
+  const map = {};
+  for (let letter in magazine) {
+    if (!map[letter]) {
+      map[letter] == 0;
     }
+    map[letter]++;
   }
-  console.log(hashTable);
-  if (Object.keys(hashTable).length == ransomNote.length) {
-    return true;
-  } else {
-    return false;
+
+  for (let letter in ransomNote) {
+    if (!map[letter]) {
+      return false;
+    }
+    map[letter]--;
   }
+  return true;
 };
 
-console.log(canConstruct("aa", "ab"));
+// if (ransomNote.length > magazine.length) {
+//     return false;
+//   }
+
+//   let hashTable = {};
+//   for (let i = 0; i < magazine.length; i++) {
+//     if (!Object.hasOwn(hashTable, magazine[i])) {
+//       hashTable[magazine[i]] = magazine
+//         .split("")
+//         .filter((x) => x == magazine[i]).length;
+//     }
+//   }
+
+//   for (let j = 0; j < ransomNote.length; j++) {
+//     if (Object.keys(hashTable).includes(ransomNote[j])) {
+//       if (hashTable[ransomNote[j]] == 0) {
+//         return false;
+//       }
+//       hashTable[ransomNote[j]] -= 1;
+//     } else {
+//       return false;
+//     }
+//   }
+//   console.log(hashTable);
+//   return true;
+
+//   let hashTable = {};
+//   for (let i = 0; i < magazine.length; i++) {
+//     if (ransomNote.includes(magazine[i])) {
+//       console.log(i);
+//       if (!Object.hasOwn(hashTable, i)) {
+//         if (
+//           Object.values(hashTable).filter((x) => x == magazine[i]).length <
+//           ransomNote.split("").filter((x) => x == magazine[i]).length
+//         ) {
+//           hashTable[i] = magazine[i];
+//           if (Object.keys(hashTable).length == ransomNote.length) {
+//             return true;
+//           }
+//         }
+//       } else {
+//         return false;
+//       }
+//     }
+//   }
+//   return false;
+// };
+
+console.log(canConstruct("a", "b"));
+// console.log(
+//   canConstruct("bg", "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj")
+// );
