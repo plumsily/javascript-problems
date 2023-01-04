2eb54d0b4b8fe57f35f6aeb8c87a3cbe2bf5dc14
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-01-04 15:29:16 -0800

    daily leetcode

--- LeetCode/20-valid-parentheses.js
+++ LeetCode/20-valid-parentheses.js
@@ -34,6 +34,11 @@
 // 1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
 
+//p: the parameters are a string, s, that contains only of (){}[]. they will be either completely closed or left open.
+//r: we must return a boolean to confirm if all the brackets are closed as true or false if one is dangling.
+//e: in the above examples, we can see that a closed bracket must be of the same type.
+//p: here, we can see that it would be useful to use a stack array, where each value is added. we can then compare the next value to the top of the stack to see if it will complete. if it can close it, we remove the top value from the stack using shift. if it does not complete, we can add that value to the stack. we use a complementary hash map object that contains the corresponding close bracket. thus, if we can match the correct complement from the top of the stack to the current value in the loop of s, we can remove it from the stack.
+
 // /**
 //  * @param {string} s
 //  * @return {boolean}
@@ -42,12 +47,16 @@ var isValid = function (s) {
   const map = { "(": ")", "{": "}", "[": "]" };
   let stack = [];
   for (let i = 0; i < s.length; i++) {
+    //check whether if it's an opening bracket
     if (Object.hasOwn(map, s[i])) {
       stack.unshift(s[i]);
+      //else if it's a closing bracket, compare it to the stack
     } else {
+      //if the stack is a complement to the current value, we remove the top of the stack
       if (map[stack[0]] === s[i]) {
         stack.shift();
       } else {
+        //we know we can return false here since the top of the stack represents the innermost bracket. if that cannot close, then we already know it won't pass.
         return false;
       }
     }
